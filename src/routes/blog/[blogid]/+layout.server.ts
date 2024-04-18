ff68d7ca5210342dfe0b6867dff1a2d4afc97312
Dawit Mekonnen <56738450+Dave-lab12@users.noreply.github.com>
2024-04-18 17:45:54 +0300

    Add error page and handle blog loading errors

--- src/routes/blog/[blogid]/+layout.server.ts
+++ src/routes/blog/[blogid]/+layout.server.ts
@@ -1,14 +1,27 @@
 import type { LayoutServerLoad } from './$types';
 import { PUBLIC_API_URL } from '$env/static/public'
+import { error } from '@sveltejs/kit';
 
 export const load = (async ({ fetch, params }) => {
 
     const { blogId } = params;
+    if (!blogId) return { status: 404 };
+    const API_URL = `${PUBLIC_API_URL}/blogs/${blogId}`;
 
-    const API_URL = PUBLIC_API_URL + `/blogs/${blogId}`;
+    try {
+        const blogs = await fetch(API_URL)
+        const blogsData = await blogs.json()
+        if (!blogsData.success) {
+            error(404, {
+                message: blogsData?.message || "Something went wrong"
+            });
+        }
+        return { blogs: blogsData };
+    } catch (error) {
+        // @ts-ignore
+        error(404, {
+            message: 'Not found'
+        });
+    }
 
-    const blogs = await fetch(API_URL)
-    const blogsData = await blogs.json()
-
-    return { blogs: blogsData };
 }) satisfies LayoutServerLoad;
