89aedcaaa3e25289556e4fb535e45ed601274e86
tensae-b <tensaeb2016@gmail.com>
2024-04-20 10:12:35 +0300

    blog content display fix

--- src/routes/blog/[blogid]/+page.svelte
+++ src/routes/blog/[blogid]/+page.svelte
@@ -19,14 +19,22 @@
 	imageUrl={blog.photo_url}
 />
 
-<section class=" mx-96 my-24">
-	<div class="hero__background bg-[url('/hero_ellipse.svg') w-full"></div>
+<section class="my-24">
+	<div
+		class="hero__background absolute bg-[url('/hero_ellipse.svg')] w-full h-[109%] bg-no-repeat right-0 top-[-67px] max-w-[797px] z-[-1] hidden md:block"
+	></div>
 
-	<div class="blog-content">
-		<h1 class="blog-heading text-4xl my-16">{@html blog[0]?.title}</h1>
-		<div class="social-icons"></div>
+	<div class="blog-content mx-4 flex flex-col items-center">
+		<h1
+			class="blog-heading font-bold text-left max-w-[820px] text-[#f2f2f2] font-montserrat text-4xl leading-normal my-8"
+		>
+			{@html blog[0]?.title}
+		</h1>
+		<div class="social-icons flex justify-end gap-2"></div>
 		<!-- <img class="heading-img" src={blog.photo_url} alt="" /> -->
-		<div class="blog-content prose prose-invert prose-a:text-cyan-700 lg:prose-xl">
+		<div
+			class="blog-content my-8 prose prose-invert prose-p:text-left prose:lg prose-p:text-[#f2f2f2] prose-p:font-montserrat prose-p:text-xl prose-p:leading-10 prose-figure:w-full prose-img:w-full prose-figcaption:text-center prose-a:text-cyan-700 md:prose-xl"
+		>
 			{@html blog[0]?.description}
 		</div>
 	</div>
