d416f375f50850b69cdf8823b83effaf21598c02
tensae-b <tensaeb2016@gmail.com>
2024-05-17 15:13:20 +0300

    no border please

--- src/lib/Modal/ModalForm.svelte
+++ src/lib/Modal/ModalForm.svelte
@@ -61,7 +61,7 @@
 	/>
 	<Dialog.Content
 		class="fixed flex flex-col left-[50%] top-[50%] border-none z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] rounded-lg
-		border bg-gradient-to-br from-[#1b3049] to-[#205d5c] p-5 shadow-popover outline-none sm:max-w-[490px] md:w-full"
+		 bg-gradient-to-br from-[#1b3049] to-[#205d5c] p-5 shadow-popover outline-none sm:max-w-[490px] md:w-full"
 	>
 		<Dialog.Title class="text-3xl mb-5 font-montserrat mt-6 text-center ">Contact Us</Dialog.Title>
 		<Separator.Root class="" />
